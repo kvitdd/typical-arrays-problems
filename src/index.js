5e307e57b49d4e4192731264784c55c828f07694
kvitdd <dianatrotskaya@gmail.com>
2021-08-18 14:59:52 +0300

    feat: complete the task typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
+    if (array) {
+        if (array.length === 0) return 0;
+        return Math.min.apply(null, array);
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array) {
+        if (array.length == 0) {
+            return 0;
+        } else {
+            return Math.max.apply(null, array);
+        }
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array) {
+        let sum = 0;
+        if (array.length == 0) {
+            return 0;
+        } else {
+            for (let i = 0; i < array.length; i++) {
+                sum += array[i];
+            }
+            return sum / array.length;
+        }
+    }
+    return 0;
+};
